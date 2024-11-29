bfd210035ee9488aa6474eb3c33056bb61bde7bc
mrozio13pl <41209892+mrozio13pl@users.noreply.github.com>
2024-11-29 21:41:32 +0100

    use lightningcss

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
     plugins: [react(), UnoCSS(), tsconfigPaths()],
+    css: {
+        transformer: 'lightningcss',
+    },
 });
