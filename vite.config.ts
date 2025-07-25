a925384e4d4f0f68b2270d493b3b27e73bfa12ed
mrozio13pl <mrozio13pl@gmail.com>
2025-07-25 14:57:26 +0200

    minify html

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-oxc';
+import html from 'vite-plugin-simple-html';
 import UnoCSS from 'unocss/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-    plugins: [react(), UnoCSS(), tsconfigPaths()],
+    plugins: [react(), UnoCSS(), tsconfigPaths(), html({ minify: true })],
     css: {
         transformer: 'lightningcss',
     },
