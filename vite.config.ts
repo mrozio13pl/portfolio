f35646a7dfa699dbdca78d5882b8b3be669bf8c7
mrozio13pl <mrozio13pl@gmail.com>
2025-07-26 00:15:24 +0200

    optimize images

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-oxc';
-import html from 'vite-plugin-simple-html';
 import UnoCSS from 'unocss/vite';
+import html from 'vite-plugin-simple-html';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-    plugins: [react(), UnoCSS(), tsconfigPaths(), html({ minify: true })],
+    plugins: [
+        react(),
+        UnoCSS(),
+        ViteImageOptimizer(),
+        tsconfigPaths(),
+        html({ minify: true })
+    ],
     css: {
         transformer: 'lightningcss',
     },
