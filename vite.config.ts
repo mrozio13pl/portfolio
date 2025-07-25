2a8b55d55b17e14f3a6b4fd06bd4fc72cebd8bea
mrozio13pl <mrozio13pl@gmail.com>
2025-07-25 12:47:41 +0200

    switch to rolldown

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react-oxc';
 import UnoCSS from 'unocss/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
